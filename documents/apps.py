6641758a56e05c02589ab3c1914f5666302c7b99
Ibrokhim <132742784+ibrodevs@users.noreply.github.com>
2026-08-07 16:24:13 +0600

    feat: enable OCR fallback after multi-pass recognition

--- documents/apps.py
+++ documents/apps.py
@@ -9,6 +9,7 @@ class DocumentsConfig(AppConfig):
         from documents.receipt_avia_brand_patch import install_receipt_avia_brand_patch
         from documents.receipt_hotel_booking_guard import install_receipt_hotel_booking_guard
         from documents.receipt_multiform_patch import install_receipt_multiform_patch
+        from documents.receipt_ocr_fallback import install_receipt_ocr_fallback
         from documents.receipt_parser_patch_safe import install_receipt_parser_patch
         from documents.receipt_preflight_patch import install_receipt_preflight_patch
         from documents.receipt_problem_formats_patch import install_receipt_problem_formats_patch
@@ -25,3 +26,4 @@ class DocumentsConfig(AppConfig):
         install_receipt_preflight_patch()
         install_receipt_quality_guard()
         install_receipt_recognition_engine()
+        install_receipt_ocr_fallback()
